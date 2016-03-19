6ee3391b8d6853b29041aea71a3c21d03d68fb9f
Thomas Wicker <thomas@thomaswicker.com>
2016-03-19 12:32:32 -0600

    updates to home page layout

--- js/application.js
+++ js/application.js
@@ -4,3 +4,37 @@ $('.grid').masonry({
   // options
   itemSelector: '.grid-item'
 });
+
+
+var navBar            = $('#nav-bar'),
+    navToggleClose    = $('#nav-bar-toggle-close'),
+    navToggle         = $('#nav-bar-toggle'),
+    searchTrigger     = $('#search-trigger'),
+    searchTriggerIcon = $('#search-trigger i'),
+    closeSearch       = $('#close-search'),
+    searchContainer   = $('#search-container');
+
+$(function () {
+  searchTrigger.on('click', function () {
+    searchContainer.toggleClass('open');
+    searchTriggerIcon.toggleClass('active');
+  })
+  closeSearch.on('click', function () {
+    searchContainer.toggleClass('open');
+    searchTriggerIcon.toggleClass('active');
+  })
+});
+
+$(function () {
+  navToggle.on('click', function () {
+    navBar.toggleClass('active');
+    navToggle.toggleClass('inactive');
+    navToggleClose.toggleClass('active');
+  })
+  navToggleClose.on('click', function () {
+    navBar.toggleClass('active');
+    navToggle.toggleClass('inactive');
+    navToggleClose.toggleClass('active');
+  })
+});
+
